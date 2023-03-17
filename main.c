36864ef6f1f538b26395c431f50652cfeda6dc08
Mohamed Essam <114494116+Enmo7@users.noreply.github.com>
2023-03-17 13:17:16 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //    register variable
+    register int var = 52;
+
+    printf("Value of Register Variable: %d\n", var);
+    return 0;
+}
